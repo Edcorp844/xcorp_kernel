9c455c14c7735775f8e6a60a52871c69dfcf0c93
Edcorp844 <edsonchan6@gmail.com>
2023-06-28 08:58:54 +0000

    update on debug.sh

--- debug.sh
+++ debug.sh
@@ -5,15 +5,25 @@ function menu() {
     GREEN='\033[0;32m'
     YELLOW='\033[1;33m'
     RED='\033[0;31m'
+    PURPLE='\033[0;35m'
     NC='\033[0m' # No color
+
+
+echo -e "${YELLOW}__  __      ____                   ____  _____ ${GREEN}____  _   _  ____ "
+echo -e "${YELLOW}\\ \\/ /     / ___|___  _ __ _ __   |  _ \\| ${GREEN}____| __ )| | | |/ ___|"
+echo -e "${YELLOW} \\  /_____| |   / _ \\| '__| '_ \\  | | | | ${GREEN}_|  |  _ \\| | | | |  _ "
+echo -e "${YELLOW} /  \\_____| |__| (_) | |  | |_) | | |_| | ${GREEN}|___| |_) | |_| | |_| |"
+echo -e "${YELLOW}/_/\\_\\     \\____\\___/|_|  | .__/  |____/|${GREEN}_____|____/ \\___/ \\____|"
+echo -e "${YELLOW}                          |_|${NC}"
+
 
     echo -e "${GREEN}Debug option:${NC}"
     echo -e "${YELLOW}[${RED}1${YELLOW}]${GREEN} Image to hex${NC}"
     echo -e "${YELLOW}[${RED}2${YELLOW}]${GREEN} fs${NC}"
     echo -e "${YELLOW}[${RED}3${YELLOW}]${GREEN} bootoptions${NC}"
-    
+
     read -p "Enter your choice: " choice
-    
+
     case $choice in
         1)
 	   ./build/tools/debug/readImage build/main_floppy.img img.hex
