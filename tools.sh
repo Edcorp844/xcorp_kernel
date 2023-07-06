e17e2f0516c685678b34ee9f604538437c2f14ad
Edcorp844 <edsonchan6@gmail.com>
2023-07-06 18:34:26 -0400

    udate

--- tools.sh
+++ tools.sh
@@ -1,5 +1,5 @@
-#sudo apt install nasm
-#sudo apt install qemu-system-x86
+sudo apt install nasm
+sudo apt install qemu-system-x86
 #sudo apt install neovim
 sudo apt-get install mtools
 sudo apt-get install dosfstools
