0c5ab28047fe29b60e7be26caeeb776171987aff
Frost Edson <77022177+Edcorp844@users.noreply.github.com>
2023-07-28 01:00:26 +0300

    Delete src/bootloader/stage2 directory

--- src/bootloader/stage2/main.c
+++
@@ -1,6 +0,0 @@
-#include "stdint.h"
-
-void __attribute__((cdecl)) start(uint16_t bootDrive, void* partition)
-{
-
-}
--- src/bootloader/stage2/stdio.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void putc(char c);
-void puts(const char* str);
