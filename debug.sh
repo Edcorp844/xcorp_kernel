70f7ec4ce0da6c8d331aa7f6c440743a98813b9f
Frost Edson <77022177+Edcorp844@users.noreply.github.com>
2023-07-28 01:14:00 +0300

    Accormodate more debug Options

--- debug.sh
+++ debug.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+build_folder="build"
+stage1_bin="stage1.bin"
+stage2_bin="stage2.bin"
+main_floppy="main_floppy.img"
+
 function menu() {
     # ANSI escape sequences for color decorations
     GREEN='\033[0;32m'
@@ -19,22 +24,49 @@ echo -e "${YELLOW}                          |_|${NC}"
 
     echo -e "${GREEN}Debug option:${NC}"
     echo -e "${YELLOW}[${RED}1${YELLOW}]${GREEN} Image to hex${NC}"
-    echo -e "${YELLOW}[${RED}2${YELLOW}]${GREEN} fs${NC}"
-    echo -e "${YELLOW}[${RED}3${YELLOW}]${GREEN} bootoptions${NC}"
+    echo -e "${YELLOW}[${RED}2${YELLOW}]${GREEN} Stage1.bin to hex${NC}"
+    echo -e "${YELLOW}[${RED}3${YELLOW}]${GREEN} Stage2.bin to hex${NC}"
+    echo -e "${YELLOW}[${RED}4${YELLOW}]${GREEN} Boot from stage1.bin${NC}"
+    echo -e "${YELLOW}[${RED}5${YELLOW}]${GREEN} Boot from floppyImage${NC}"
 
     read -p "Enter your choice: " choice
 
     case $choice in
         1)
-	   ./build/tools/debug/readImage build/main_floppy.img img.hex
+            # Check if the file exists in the build folder
+            if [ -e "$build_folder/$main_floppy" ]; then
+                 ./build/tools/debug/readImage $build_folder/$main_floppy img.hex
+            else
+                 echo -e "${RED}[X] Error. ${NC}$main_floppy does not exist."
+            fi
             ;;
         2)
-            echo -e "${GREEN}You chose 'fs'${NC}"
-            # Add your code for 'fs' here
+            if [ -e "$build_folder/$stage1_bin" ]; then
+                 ./build/tools/debug/readImage $build_folder/$stage1_bin stage1.hex
+            else
+                 echo -e "${RED}[X] Error. ${NC}$stage1_bin does not exist."
+            fi
             ;;
         3)
-            echo -e "${GREEN}You chose 'bootoptions'${NC}"
-            # Add your code for 'bootoptions' here
+            if [ -e "$build_folder/$stage2_bin" ]; then
+                 ./build/tools/debug/readImage $build_folder/$stage2_bin stage2.hex
+            else
+                 echo -e "${RED}[X] Error. ${NC}$stage2_bin does not exist."
+            fi
+            ;;
+        4)
+           if [ -e "$build_folder/$stage1_bin" ]; then
+                 qemu-system-x86_64 -hda $build_folder/$stage1_bin
+            else
+                 echo -e "${RED}[X] Error. ${NC}$stage1_bin does not exist."
+            fi
+            ;;
+        5)
+           if [ -e "$build_folder/$main_floppy" ]; then
+                 qemu-system-x86_64 -fda $build_folder/$main_floppy
+            else
+                 echo -e "${RED}[X] Error. ${NC}$main_floppy does not exist."
+            fi
             ;;
         *)
             echo -e "${RED}Invalid choice. Please enter a valid option.${NC}"
