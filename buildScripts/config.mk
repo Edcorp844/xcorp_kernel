926c5bf07399380b753279d53c42b6f832293c41
Frost Edson <77022177+Edcorp844@users.noreply.github.com>
2023-07-28 01:15:57 +0300

    Delete buildScripts directory

--- buildScripts/config.mk
+++
@@ -1,8 +0,0 @@
-
-TARGET = i686-elf
-
-BINUTILS_VERSION = 2.37
-BINUTILS_URL = https://ftp.gnu.org/gnu/binutils/binutils-$(BINUTILS_VERSION).tar.xz
-
-GCC_VERSION = 11.2.0
-GCC_URL = https://ftp.gnu.org/gnu/gcc/gcc-11.2.0/gcc-$(GCC_VERSION).tar.xz
